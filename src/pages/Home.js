73a84aa7a430112610ba6e91e2ee2546afd38f90
lantanatj <lantanat62@gmail.com>
2022-09-08 08:38:42 +0100

    added a loader to the tasks page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,68 @@
 import React, { useEffect, useState } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import HashLoader from 'react-spinners/HashLoader'
 
 
 
 const Home = () => {
     const [todo, setTodo] = useState(null)
+    const [loading, setLoading] = useState(false)
 
-    const getdata = ()=>{
+    const getdata = () => {
         fetch("https://node-todo-app45.herokuapp.com/todo/")
-        .then((res) => res.json())
-        .then((data)=> setTodo(data.todos))
-      }
-  
-      useEffect(()=>{
-            getdata()
-  
-        
-      }, [])
-  
+            .then((res) => res.json())
+            .then((data) => setTodo(data.todos))
+    }
 
-  return (
-    <div className='todo_body'>
-        <div className='mappedItem'>
-            {todo &&  <> {todo.map((data)=>(
-                <div  key={data._id}>
-                    <h1>{data.title}</h1>
-                    <p>{data.body}</p>
+    useEffect(() => {
 
-                    <Link to={`/${data._id}`}>Read more</Link>
-                    <hr/>
-                    
-                </div>
-            ))}</>}
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 7000)
+
+        getdata()
+
+
+    }, [])
+   
+
+    return (
+
+
+        <div className="App">
+            {
+                loading ?
+
+                    <div className="loaders">
+                        <HashLoader
+                            className='loader'
+                            size={50}
+                            background='transparent'
+                            color={"#3d2c8d"}
+                            loading={loading}
+                        />
+                    </div>
+                    :
+                    <div className='todo_body'>
+                        <div className='mappedItem'>
+                            {todo && <> {todo.map((data) => (
+                                <div key={data._id}>
+                                    <h1>{data.title}</h1>
+                                    <p>{data.body}</p>
+
+                                    <Link to={`/${data._id}`}>Read more</Link>
+                                    <hr />
+
+                                </div>
+                            ))}</>}
+                        </div>
+
+                    </div>
+            }
+
         </div>
-        
-    </div>
-  )
+    )
 }
 
 export default Home
